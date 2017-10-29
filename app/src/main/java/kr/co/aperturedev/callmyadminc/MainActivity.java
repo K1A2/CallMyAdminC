2f0313621bc1eef01b289e270678091671668e5a
K1A2 <jckim0307@gmail.com>
2017-10-29 19:01:50 +0900

    Test

--- app/src/main/java/kr/co/aperturedev/callmyadminc/MainActivity.java
+++ app/src/main/java/kr/co/aperturedev/callmyadminc/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Hello world!!!");
         System.out.println("Hello world!!!");
         System.out.println("Hello world!!!");
-        //w주석주석
+        //w주석주석dddd
     }
 }
