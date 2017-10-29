df99a43d5c25590f3b86190446c64341b117d4b2
K1A2 <jckim0307@gmail.com>
2017-10-29 20:51:35 +0900

    Test

---
+++ app/src/main/java/kr/co/aperturedev/callmyadminc/HttpRequester.java
@@ -0,0 +1,89 @@
+package kr.co.aperturedev.callmyadminc;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by jckim on 2017-10-29.
+ */
+
+public class HttpRequester extends Thread {
+    private JSONObject jsonObj = null;
+    private String reqURL = null;
+    private OnHttpResponse listener = null;
+
+    public HttpRequester(JSONObject jsonObj, String reqURL) {
+        this.jsonObj = jsonObj;
+        this.reqURL = reqURL;
+    }
+
+    public void setListener(OnHttpResponse listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void run() {
+        HttpURLConnection conn = null;
+        JSONObject resJsonObj = null;
+        boolean isSucc = false;
+
+        try {
+            // 커넥션을 만듭니다.
+            URL url = new URL(this.reqURL);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Content-Length", this.jsonObj.toString().length() + "");
+            conn.setRequestProperty("Content-Language", "ko-KR");
+            conn.setUseCaches(false);
+            conn.setDoOutput(true);
+
+            // 요청
+            String parameter = "data=" + URLEncoder.encode(this.jsonObj.toString(), "EUC-KR");
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(parameter);
+            wr.flush();
+
+            // 응답
+            InputStream in = conn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            StringBuilder response = new StringBuilder();
+            String line = "";
+
+            while((line = reader.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+
+            reader.close();
+
+            Log.e("Respond", "Http response : \n " + response.toString());
+
+            resJsonObj = new JSONObject(response.toString());
+            isSucc = true;
+        } catch(Exception ex) {
+            Log.e("Error", "HttpRequest Error!!!");
+            ex.printStackTrace();
+            isSucc = false;
+        }
+
+        if(this.listener != null) {
+            // 리스너가 등록된 상태
+            this.listener.onResponse(isSucc, resJsonObj);
+        }
+    }
+
+    public interface OnHttpResponse {
+        void onResponse(boolean isSucc, JSONObject jsonObj);
+    }
+}
--- app/src/main/java/kr/co/aperturedev/callmyadminc/MainActivity.java
+++ app/src/main/java/kr/co/aperturedev/callmyadminc/MainActivity.java
@@ -1,7 +1,18 @@
 package kr.co.aperturedev.callmyadminc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +21,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Hello world!!!");
-        System.out.println("Hello world!!!");
-        System.out.println("Hello world!!!");
-        //w주석주석dddd
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            int permisionRequest = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_NETWORK_STATE);
+
+            if (permisionRequest == PackageManager.PERMISSION_DENIED)
+                if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_NETWORK_STATE)||shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_NETWORK_STATE)) {
+                    AlertDialog.Builder permissioCheck = new AlertDialog.Builder(MainActivity.this);
+                    permissioCheck.setTitle("권한요청")
+                            .setMessage("권한요청")
+                            .setCancelable(false)
+                            .setPositiveButton("동의", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE}, 1000);
+                                    }
+                                }
+                            })
+                            .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(MainActivity.this, "앱 사용이 불가합니다", Toast.LENGTH_SHORT).show();
+                                    start();
+                                }
+                            })
+                            .create()
+                            .show();
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+                } else {
+                start();
+            }
+        } else {
+            start();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 100:
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    start();
+                } else {
+                    Toast.makeText(MainActivity.this, "앱을 사용할수 없습니다", Toast.LENGTH_SHORT).show();
+                    start();
+                }
+                return;
+        }
+    }
+
+    private void start() {
+        String str =
+                "[{'name':'jjj','age':43,'address':'ajjja'},"+
+                        "{'name':'aaa','age':36,'address':'kaaak'},"+
+                        "{'name':'bbb','age':25,'address':'nbbbn'}]";
+        try {
+            JSONObject a = new JSONObject(str);
+            HttpRequester httpRequester = new HttpRequester(a, "http://");
+            httpRequester.run();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
