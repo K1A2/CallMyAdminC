d83d01adc7d56578b741d64299bdd4eaa14a45dc
ballbot <5252bb@daum.net>
2017-10-29 18:44:04 +0900

    Hello world commit

--- app/src/main/java/kr/co/aperturedev/callmyadminc/MainActivity.java
+++ app/src/main/java/kr/co/aperturedev/callmyadminc/MainActivity.java
@@ -1,7 +1,7 @@
 package kr.co.aperturedev.callmyadminc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
         System.out.println("Hello world!!!");
     }
 }
